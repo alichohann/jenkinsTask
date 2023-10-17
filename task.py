ac49a59581f80db1632449d0eada6446dd513698
Ali Chohan <90602926+alichohann@users.noreply.github.com>
2023-10-17 10:49:34 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,11 +1,11 @@
 class Calculator:
     def add(self, a, b):
         """Add two numbers."""
-        return a + b
+        return a+b
 
     def subtract(self, a, b):
         """Subtract b from a."""
-        return a - b
+        return a-b
 
     def multiply(self, a, b):
         """Multiply two numbers."""
